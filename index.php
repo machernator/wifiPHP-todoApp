856b725a14d6fa4514a7dcdaa2dfeece2ecc4240
WIFI-IT\machert <machernator@gmail.com>
2017-10-03 13:49:58 +0200

    Style tag entfernt

--- index.php
+++ index.php
@@ -9,11 +9,6 @@ require_once 'inc/init.inc.php';
     <title>MySQL Einstieg</title>
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
     <link rel="stylesheet" href="css/todo.css">
-    <style>
-    body {
-        padding: 1em;
-    }
-    </style>
 </head>
 <body>
     <h1>Todo Liste aus MySQL</h1>
