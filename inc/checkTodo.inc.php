f3adb2162833b54bd544f619868feaf8145ba637
WIFI-IT\machert <machernator@gmail.com>
2017-10-10 10:31:57 +0200

    Todo inaktiv gesetzt

--- inc/checkTodo.inc.php
+++ inc/checkTodo.inc.php
@@ -6,6 +6,14 @@ if (count($_GET) > 0 &&
     $doneid = mysqli_escape_string($mysql, $_GET['doneid']);
 
     // Auf inaktiv (0) setzen
-
+    $switchSql = "UPDATE todos SET active=0 WHERE todos_id=$doneid";
+    $switchRes = mysqli_query($mysql, $switchSql);
+    
+    if ($switchRes) {
+        $msg = '<p class="success">Todo erledigt.</p>';
+    }
+    else {
+        $msg = '<p class="warning">Fehler, todo konnte nicht erledigt werden.</p>';
+    }
    
 }
